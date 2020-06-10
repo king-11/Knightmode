e48e1e076fd9a3e315a7dc6c1b2c35e4e77b01e4
king-11 <lakshay.singh1108@gmail.com>
2020-06-10 16:14:52 +0530

    allowed host and url update

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name='blog'
+app_name = 'blog'
 
 urlpatterns = [
-    path('welcome/', views.welcome, name='welcome'),
+    path('', views.welcome, name='welcome'),
     path('calendar/', views.calendar, name='Calendar'),
     path('identity/', views.identity, name='identity'),
     path('curriculum/', views.curriculum, name='curriculum'),
